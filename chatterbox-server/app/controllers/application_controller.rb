69576abd768e7a770cf667a1365463189d064ea0
Patrick <ruomungai@gmail.com>
2022-09-05 20:00:46 +0300

    commit tree 2b9f81949c871fa30ab35d9bd1c7573e19c3cf8c

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do
+    mes=Message.all
+    mes.to_json
+  end 
+
+  post '/messages' do 
+    mes=Message.create(
+      body: params[:body], 
+      username: params[:username]
+    )
+    mes.to_json
+  end
+
+  patch '/messages/:id' do
+    pat=Message.find(params[:id])
+    pat.update(body: params[:body])
+    pat.to_json
+  end
+
+  delete '/messages/:id' do
+    del=Message.find(params[:id])
+    del.destroy
+    del.to_json
+  end
+
+   
 end
